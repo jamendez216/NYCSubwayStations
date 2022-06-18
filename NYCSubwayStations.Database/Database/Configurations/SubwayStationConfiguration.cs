85078356840ac5dcdb65a992da4d307d24db0000
José Armando Méndez Espinal <jamendez216@gmail.com>
2022-06-18 18:00:51 -0400

    Configuration files for database models

---
+++ NYCSubwayStations.Database/Database/Configurations/SubwayStationConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NYCSubwayStations.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NYCSubwayStations.Database.Database.Configurations
+{
+    public class SubwayStationConfiguration : IEntityTypeConfiguration<SubwayStation>
+    {
+        public void Configure(EntityTypeBuilder<SubwayStation> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Id)
+                .ValueGeneratedNever();
+
+            builder.HasIndex(e => new { e.Latitude, e.Longitude })
+                .IsUnique();
+
+        }
+    }
+}
